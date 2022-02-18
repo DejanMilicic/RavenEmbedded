cd88990ba84e8d382835f79d91d55ab96de05870
Dejan Milicic <dejan.milicic@gmail.com>
2022-02-18 20:44:18 +0100

    example of using embedded database

---
+++ Emb/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Emb;
+using Raven.Embedded;
+
+Console.WriteLine("Hello, World!");
+
+//EmbeddedServer.Instance.StartServer();
+EmbeddedServer.Instance.StartServer(new ServerOptions
+{
+    DataDirectory = "C:\\RavenData",
+    ServerUrl = "http://127.0.0.1:8090"
+});
+
+
+using (var store = EmbeddedServer.Instance.GetDocumentStore("Embedded"))
+{
+    using (var session = store.OpenSession())
+    {
+        session.Store(new User
+        {
+            Name = "Jane Smith"
+        });
+
+        session.SaveChanges();
+    }
+
+    Console.ReadLine();
+}
